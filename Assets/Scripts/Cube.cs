cbf8068781b78aa631adcdc14557acffbda0c149
RmCalozz <xander.18lozano@gmail.com>
2018-10-11 14:09:18 +0200

    Update Assets/Scripts/Cube.cs

--- Assets/Scripts/Cube.cs
+++ Assets/Scripts/Cube.cs
@@ -4,14 +4,10 @@ using UnityEngine;
 
 public class Cube : MonoBehaviour {
 
-<<<<<<< HEAD
-
     public Vector3 direction = Vector3.back;
     public float speed = 1;
 
     void Update () {
         this.transform.Translate(Vector3.back * this.speed * Time.deltaTime);
-
-
     }
 }
