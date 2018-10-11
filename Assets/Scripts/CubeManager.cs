d216b0fb20c498afa86186391d9c3549d428ddf2
InterDan <Inteternenet-2@hotmail.es>
2018-10-11 13:39:02 +0200

    Añadido CubeManager
    Se encarga de spawnear una nueva oleada de cubos y de administrarlos.
    
    SpawnCubes(List<Transform> positions) spawnea los cubos dada una lista de posiciones
    
    AreStillCubes() comprueba si todavía hay cubos en la escena.

--- Assets/Scripts/CubeManager.cs
+++ Assets/Scripts/CubeManager.cs
@@ -4,15 +4,29 @@ using UnityEngine;
 
 public class CubeManager : MonoBehaviour {
 
+    public GameObject cubePrefab;
     List<Cube> currentCubes = new List<Cube>();
+
+    public void Start()
+    {
+        
+    }
 
     public void SpawnCubes(List<Transform>positions)
     {
-       
+        int randIndex = Random.Range(0, positions.Count);
+        for(int i = 0; i < positions.Count; i++)
+        {
+            if (i != randIndex)
+            {
+                Cube newCube = Instantiate(cubePrefab, positions[i].position, positions[i].rotation).GetComponent<Cube>();
+                currentCubes.Add(newCube);
+            }
+        }
     }
 
-    void Update () {
-		
-       
-	}
+    public bool AreStillCubes()
+    {
+        return currentCubes.Count != 0;
+    }
 }
