c7bbd43f3ed4398b0b0ce96339d1c19b250c009d
RmCalozz <xander.18lozano@gmail.com>
2018-10-11 14:38:30 +0200

    Deteccion y destruccion de cubos

---
+++ Assets/Scripts/DetectorCubos.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DetectorCubos : MonoBehaviour {
+
+    public int score;
+
+   void OnTriggerEnter(Collider other)
+   {
+        Debug.Log("detected");
+      if (other.gameObject.tag == "detector")
+      {
+            Destroy(other.gameObject);
+            GameManager.CubeDestroyed();
+      }
+   }
+}
