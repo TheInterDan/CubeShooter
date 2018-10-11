47742275dc541f34b73f9867f20d0e619123f4e9
javiabella <javiabella86@gmail.com>
2018-10-11 13:18:45 +0200

    añadido scripts CubeManager y Cube

---
+++ Assets/Scripts/CubeManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeManager : MonoBehaviour {
+
+    List<Cube> currentCubes = new List<Cube>();
+
+    public void SpawnCubes(List<Transform>positions)
+    {
+       
+    }
+
+    void Update () {
+		
+       
+	}
+}
