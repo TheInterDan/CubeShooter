0d10de3d9bf684c3cb1953f8556892f37afd4090
javiabella <javiabella86@gmail.com>
2018-10-11 14:04:35 +0200

    Player
    Detecta el input del jugador
    Mueve el jugador a la posición seleccionada.

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    public int speed;
+
+    Transform destino;
+
+    void Update() {
+
+        DetectInput();
+        Move();
+    }
+    void DetectInput()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out hit, Mathf.Infinity))
+            {
+                if (hit.transform.tag == "PlayerPosition")
+                {
+                    destino = hit.transform;
+                }
+            }
+        }
+    }
+    void Move()
+    {
+        this.transform.position = Vector3.Lerp(this.transform.position, this.destino.position, this.speed * Time.deltaTime);
+    }
+}
