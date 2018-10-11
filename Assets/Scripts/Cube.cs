64fb29f9cc17e034b9582dd361d7ceddac0a0633
RmCalozz <xander.18lozano@gmail.com>
2018-10-11 14:06:51 +0200

    Añadido cubo (enemigo)

---
+++ Assets/Scripts/Cube.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cube : MonoBehaviour {
+
+
+    public Vector3 direction = Vector3.back;
+    public float speed = 1;
+
+    void Update () {
+        this.transform.Translate(Vector3.back * this.speed * Time.deltaTime);
+
+
+    }
+}
