3cbaf2b521dc2e6363bfe4ecaa7ba0c5f05564f4
RmCalozz <xander.18lozano@gmail.com>
2018-10-15 10:39:08 +0200

    Cambiado color cubos

--- Assets/Scripts/Cube.cs
+++ Assets/Scripts/Cube.cs
@@ -5,13 +5,14 @@ using UnityEngine;
 
 
 public class Cube : MonoBehaviour {
-
+    
     public Vector3 direction = Vector3.back;
     public float speed = 1;
     public float speedMultiplier = 0.2f;
 
     private void Start()
     {
+        Random.ColorHSV();
         speed = this.speed + speedMultiplier * GameManager.Score;
     }
 
