c2d1ce2adbb9c6b8f79322ac3fb383c6bc264410
RmCalozz <xander.18lozano@gmail.com>
2018-10-15 09:52:42 +0200

    Añadido rotacion de los cubos

---
+++ Assets/Scripts/GirarCubos.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GirarCubos : MonoBehaviour {
+
+    public float speed; 
+
+	void Update () {
+        transform.Rotate(0, 90 + Time.deltaTime, 0);
+
+	}
+}
